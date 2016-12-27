b6aeb6f9fd10c6dbcd73bb949d873e3358d25d8a
Chezzy <chezrye@gmail.com>
2016-12-27 05:33:17 -0500

    Added WebsiteBase abstract class

---
+++ ProjectHEDio/Website.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProjectHEDio
+{
+    public abstract class WebsiteBase
+    {
+        protected static Queue<string> WebsiteImageLinks = new Queue<string>(); 
+
+        protected Thread DownloadThread;
+
+        public abstract int GetMaxPages(string[] arguments = null);
+
+        public abstract string FormatURL(string[] arguments = null, int pageNumber = 1);
+    }
+}
