8f3e73aed46eecac6b2388d8956c52304c0b0d5e
Chezzy <chezrye@gmail.com>
2016-12-27 05:32:49 -0500

    Added LogHelper.cs

---
+++ ProjectHEDio/LogHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ProjectHEDio
+{
+    public static class LogHelper
+    {
+        private static string LogFilepath = Environment.CurrentDirectory + "\\" + "ProjectHEDLog.txt";
+
+        public enum LogType
+        {
+            Normal,
+            Warning,
+            Error
+        }
+
+        public static void Log(string text, LogType type = LogType.Normal)
+        {
+            try
+            {
+                string result = DateTime.Now.ToString();
+                result += " ";
+                switch (type)
+                {
+                    case LogType.Normal:
+                        result += "INFO:  ";
+                        break;
+                    case LogType.Warning:
+                        result += "WARN:  ";
+                        break;
+                    case LogType.Error:
+                        result += "ERROR: ";
+                        break;
+                }
+                result += text;
+                File.AppendAllText(LogFilepath, result + Environment.NewLine);
+            }
+            catch (Exception)
+            {
+                // ??? :l
+            }
+        }
+    }
+}
