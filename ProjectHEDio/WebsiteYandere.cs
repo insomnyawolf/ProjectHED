91b44ba524aac0ea5fb328cfd1dd7a7d6973a188
Chezzy <chezrye@gmail.com>
2016-12-30 04:28:40 -0500

    Added Yandere website support (incomplete)

---
+++ ProjectHEDio/WebsiteYandere.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ProjectHEDio
+{
+    class WebsiteYandere : WebsiteBase
+    {
+        public override void InitializeScrape(string[] arguments = null, int totalPages = 1)
+        {
+            ScrapeThread = new Thread(() => Scrape(arguments, totalPages));
+            ScrapeThread.IsBackground = true;
+            ScrapeThread.Start();
+        }
+
+        protected override int GetMaxPages(string[] arguments = null)
+        {
+            string source = GetSource(FormatURL(arguments));
+
+            if (source.Contains("Nobody here but us chickens!"))
+            {
+                return 0;
+            }
+            if (!source.Contains("Next Page"))
+            {
+                return 1;
+            }
+
+            string pattern = "page=(\\d+)[^\"]+\" ?rel=\"last\"";
+            string match = Regex.Match(source, pattern).Groups[1].Value;
+            int result = -1;
+            if (int.TryParse(match, out result))
+            {
+                return result;
+            }
+            return -1;
+        }
+
+        protected override string FormatURL(string[] arguments = null, int pageNumber = 1)
+        {
+            return string.Format("http://yande.re/post?page={0}&tags={1}", pageNumber, GetTagList(arguments));
+        }
+
+        protected override void Scrape(string[] arguments = null, int totalPages = 1)
+        {
+            // TODO: This
+            throw new NotImplementedException();
+        }
+    }
+}
