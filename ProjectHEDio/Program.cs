e5213fea5e481d8113ca0dce75e83a0f1fdecab7
Chezzy <chezrye@gmail.com>
2016-12-31 23:41:24 -0500

    Dumb download test from KonachanNet

--- ProjectHEDio/Program.cs
+++ ProjectHEDio/Program.cs
@@ -8,6 +8,8 @@ namespace ProjectHEDio
 {
     static class Program
     {
+        private static List<System.Net.WebClient> WebClientList = new List<System.Net.WebClient>();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,6 +19,52 @@ namespace ProjectHEDio
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             LogHelper.ClearLogFile();
+
+            string[] tags = { "kaname_madoka" };
+            WebsiteKonachanNet wkc = new WebsiteKonachanNet();
+            wkc.InitializeScrape(tags, Int32.MaxValue);
+            while (wkc.ThreadIsAlive())
+            {
+                while (WebsiteBase.WebsiteImageLinks.Count > 0)
+                {
+                    string url = WebsiteBase.WebsiteImageLinks.Dequeue();
+                    try
+                    {
+                        Uri uri = new Uri(url);
+                        WebClientList.Add(new System.Net.WebClient());
+                        if (!System.IO.Directory.Exists(@"C:\Users\moech\Desktop" + "\\test"))
+                        {
+                            System.IO.Directory.CreateDirectory(@"C:\Users\moech\Desktop" + "\\test");
+                        }
+                        string filename = Uri.UnescapeDataString(url.Split('/')[url.Split('/').Length - 1]).Replace("Konachan.com - ", "");
+                        WebClientList.Last().DownloadFileAsync(uri, @"C:\Users\moech\Desktop" + "\\test\\" + filename);
+                        LogHelper.Log("Started download: " + filename);
+                    }
+                    catch (Exception)
+                    {
+                        LogHelper.Log("Couldn't download " + url + "!", LogHelper.LogType.Error);
+                    }
+                }
+            }
+            bool isDone = false;
+            while (!isDone)
+            {
+                isDone = true;
+                foreach (System.Net.WebClient wc in WebClientList)
+                {
+                    if (wc.IsBusy)
+                    {
+                        isDone = false;
+                        break;
+                    }
+                }
+            }
+            foreach (System.Net.WebClient wc in WebClientList)
+            {
+                wc.Dispose();
+            }
+            LogHelper.Log("Done!");
+            MessageBox.Show("done");
         }
     }
 }
