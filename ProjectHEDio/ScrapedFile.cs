0d426c112f14b222157236fdbd948befdb934c3c
Chezzy <chezrye@gmail.com>
2017-01-04 05:34:58 -0500

    Add constructor and dimension fields

--- ProjectHEDio/ScrapedFile.cs
+++ ProjectHEDio/ScrapedFile.cs
@@ -6,6 +6,8 @@ namespace ProjectHEDio
     {
         private string Link;
         private int Retries = 0;
+        private int Width = -1;
+        private int Height = -1;
 
         public ScrapedFile()
         {
@@ -21,6 +23,13 @@ namespace ProjectHEDio
         {
             this.Link = link;
             this.Retries = retries;
+        }
+
+        public ScrapedFile(string link, int width, int height)
+        {
+            this.Link = link;
+            this.Width = width;
+            this.Height = height;
         }
     }
 }
