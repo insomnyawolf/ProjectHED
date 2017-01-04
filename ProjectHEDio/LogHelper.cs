5a6973767e7d5ad182223e3e6828eb948621351a
Chezzy <chezrye@gmail.com>
2017-01-04 01:17:02 -0500

    Resolved #2
    Enclosed the Log() method in a lock block

--- ProjectHEDio/LogHelper.cs
+++ ProjectHEDio/LogHelper.cs
@@ -7,6 +7,7 @@ namespace ProjectHEDio
     public static class LogHelper
     {
         private static string LogFilepath = Environment.CurrentDirectory + "\\" + "ProjectHEDLog.txt";
+        private static object fileLock = new object();
 
         public static void ClearLogFile()
         {
@@ -22,29 +23,32 @@ namespace ProjectHEDio
 
         public static void Log(string text, LogType type = LogType.Normal)
         {
-            try
+            lock (fileLock)
             {
-                string result = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff");
-                result += " ";
-                switch (type)
+                try
                 {
-                    case LogType.Normal:
-                        result += "INFO:  ";
-                        break;
-                    case LogType.Warning:
-                        result += "WARN:  ";
-                        break;
-                    case LogType.Error:
-                        result += "ERROR: ";
-                        break;
+                    string result = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff");
+                    result += " ";
+                    switch (type)
+                    {
+                        case LogType.Normal:
+                            result += "INFO:  ";
+                            break;
+                        case LogType.Warning:
+                            result += "WARN:  ";
+                            break;
+                        case LogType.Error:
+                            result += "ERROR: ";
+                            break;
+                    }
+                    result += text;
+                    Debug.WriteLine(result);
+                    File.AppendAllText(LogFilepath, result + Environment.NewLine);
                 }
-                result += text;
-                Debug.WriteLine(result);
-                File.AppendAllText(LogFilepath, result + Environment.NewLine);
-            }
-            catch (Exception)
-            {
-                // ??? :l
+                catch (Exception)
+                {
+                    // ??? :l
+                }
             }
         }
     }
