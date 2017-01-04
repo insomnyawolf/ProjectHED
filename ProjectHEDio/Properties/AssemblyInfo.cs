8f1f4b5acff0d2e2502ae3a7937de88b7c6db01d
Chezzy <chezrye@gmail.com>
2017-01-04 05:56:46 -0500

    Change assembly attributes

--- ProjectHEDio/Properties/AssemblyInfo.cs
+++ ProjectHEDio/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProjectHEDio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ProjectHED")]
+[assembly: AssemblyDescription("An open-source bulk image downloader for anime.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProjectHEDio")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ProjectHED")]
+[assembly: AssemblyCopyright("Copyright © Chezzy 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
