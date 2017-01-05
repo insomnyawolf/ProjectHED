f98a15dc8be8c85cdd78ecb0de005bed7957a739
Chezzy <chezrye@gmail.com>
2017-01-04 21:25:55 -0500

    Set progress bar value to 0 in constructor

--- ProjectHEDio/FormMain.cs
+++ ProjectHEDio/FormMain.cs
@@ -17,6 +17,7 @@ namespace ProjectHEDio
         {
             InitializeComponent();
             comboBoxLanguageSelector.Text = "English";
+            progressBarMain.Value = 0;
         }
 
         private void metroButtonStart_Click(object sender, EventArgs e)
