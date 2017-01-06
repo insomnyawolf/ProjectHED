334465ad65994a4f7f378030bcc356a6087ce521
Chezzy <chezrye@gmail.com>
2017-01-06 01:23:06 -0500

    Tidy TODO.cs

--- ProjectHEDio/Todo.cs
+++ ProjectHEDio/Todo.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectHEDio
+﻿namespace ProjectHEDio
 {
-    static class Todo
+    internal static class Todo
     {
         //===================================================
         // Functionality
@@ -26,8 +20,8 @@ namespace ProjectHEDio
 
         //===================================================
         // Misc
-        // https://danbooru.donmai.us/wiki_pages/58938
-        // http://pixiv.readthedocs.io/en/latest/api.html
+        // - https://danbooru.donmai.us/wiki_pages/58938
+        // - http://pixiv.readthedocs.io/en/latest/api.html
         //===================================================
     }
 }
